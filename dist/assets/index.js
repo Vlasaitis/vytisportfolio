39d747c702dab1a4675a205695b38c6ee53426ea
Vytis <v.lasaitis@gmail.com>
2023-03-01 12:40:14 +0100

    snake added

---
+++ dist/assets/index.js
@@ -0,0 +1,11 @@
+export const coach = "/assets/coach.png";
+export const lg = "/assets/lg.png";
+export const logo = "/assets/logo.png";
+export const mayhem = "/assets/mayhem.png";
+export const mayhem2 = "/assets/mayhem2.png";
+export const small = "/assets/small.png";
+export const menu = "/assets/menu.svg";
+export const close = "/assets/close.svg";
+export const office = "/assets/office.png";
+export const portfolio = "/assets/portfolio.png";
+export const pokeblitz = "/assets/pokeblitz.png";
